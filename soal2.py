b67cdd90982f96fac84de129a4239975abe79c06
Fitrah Andhika Ramadhan <102272914+Skybluve@users.noreply.github.com>
2023-12-30 09:24:50 +0700

    Update soal2.py

--- soal2.py
+++ soal2.py
@@ -1,28 +1,22 @@
-def cek_palindrom(teks):
-    # Mengubah teks menjadi lowercase dan mengabaikan karakter selain huruf
-    teks = ''.join(c.lower() for c in teks if c.isalpha())
+def is_palindrome(text):
+    # Mengubah teks menjadi huruf kecil dan menghapus karakter non-alfabet
+    cleaned_text = ''.join(filter(str.isalpha, text)).lower()
     
-    # Mengecek apakah teks palindrom atau tidak
-    if teks == teks[::-1]:
-        return "eureeka!"
+    # Mengecek apakah teks adalah palindrom
+    return cleaned_text == cleaned_text[::-1]
+
+# Daftar kalimat untuk diperiksa
+kalimat = [
+    "Angsa",
+    "KataK",
+    "kasur empuk",
+    "Aku Suka Kamu",
+    "Ibu Ratna antar ubi."
+]
+
+# Memeriksa setiap kalimat dan mencetak output yang sesuai
+for teks in kalimat:
+    if is_palindrome(teks):
+        print(teks + " eureeka!")
     else:
-        return "suka blyat"
-
-# Contoh penggunaan dengan beberapa kalimat
-kalimat_1 = "Angsa suka blyat"
-kalimat_2 = "KataK eureeka!"
-kalimat_3 = "kasur empuk suka blyat"
-kalimat_4 = "Aku Suka Kamu ?"
-kalimat_5 = "Ibu Ratna antar ubi. ?"
-
-output_1 = cek_palindrom(kalimat_1)
-output_2 = cek_palindrom(kalimat_2)
-output_3 = cek_palindrom(kalimat_3)
-output_4 = cek_palindrom(kalimat_4)
-output_5 = cek_palindrom(kalimat_5)
-
-print(kalimat_1, "-", output_1)
-print(kalimat_2, "-", output_2)
-print(kalimat_3, "-", output_3)
-print(kalimat_4, "-", output_4)
-print(kalimat_5, "-", output_5)
+        print(teks + " suka blyat")
